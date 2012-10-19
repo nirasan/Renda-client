950efa7dcee3cbff0ad63b6dcb8e76b601eb2b5e
nirasan <niranirao@gmail.com>
2012-10-19 22:36:31 +0900

    ログイン処理のリファクタリング

--- src/com/example/renda/LoginActivity.java
+++ src/com/example/renda/LoginActivity.java
@@ -1,119 +1,74 @@
 package com.example.renda;
 
-import java.io.IOException;
-
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpResponseException;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.HttpStatus;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends Activity {
-    
-    private ProgressDialog progressDialog;
-    String tag = "LoginActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    
+    public void loginButtonOnClick(View v) {
         
-        ((Button)findViewById(R.id.button1)).setOnClickListener(new OnClickListener() {
+        final SharedPreferences preferences = getSharedPreferences("user", MODE_PRIVATE);
+        
+        // ユーザー名とパスワードを入力欄から取得
+        final String username = ((EditText)findViewById(R.id.editTextUsername)).getText().toString();
+        final String password = ((EditText)findViewById(R.id.editTextPassword)).getText().toString();
+        
+        // 取得できなければエラーの表示
+        if (username.equals("") && password.equals("")) {
+            ((TextView)findViewById(R.id.textView1)).setText("user not found");
+            return;
+        }
+        
+        new AsyncTaskWithDialog<Http.Result>(this) {
             
             @Override
-            public void onClick(View v) {
-                
-                // ロード中のダイアログを表示
-                progressDialog = new ProgressDialog(LoginActivity.this);
-                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                progressDialog.setCancelable(true);
-                progressDialog.setMessage("ロード中");
-                progressDialog.show();
-                
-                // バックグラウンドでサーバー通信をおこなう
-                new AsyncTask<Void, Void, Void>() {
-                    
-                    @Override
-                    protected Void doInBackground(Void...voids) {
-                        
-                        // ユーザー名とパスワードを入力欄から取得
-                        String username = ((EditText)findViewById(R.id.editTextUsername)).getText().toString();
-                        String password = ((EditText)findViewById(R.id.editTextPassword)).getText().toString();
-                        
-                        // 取得できなければ通信しない
-                        if (username.equals("") && password.equals("")) {
-                            return null;
-                        }
-                        
-                        // ユーザーの最新情報を参照するURLの作成
-                        Uri.Builder builder = new Uri.Builder();
-                        builder.scheme("http");
-                        builder.encodedAuthority("10.0.2.2:3000");
-                        builder.path("/user/show");
-                        builder.appendQueryParameter("username", username);
-                        builder.appendQueryParameter("password", password);
-                        
-                        // ユーザーの最新情報を参照
-                        HttpGet request = new HttpGet(builder.build().toString());
-                        DefaultHttpClient httpClient = new DefaultHttpClient();
-                        ResponseHandler<String> responseHandler = new BasicResponseHandler();
-                        
+            protected Http.Result doInBackground(Void...voids) {
+                // ユーザーの最新情報をサーバーに問い合わせる
+                String uri = UriBuilder.user_show_url(username, password);
+                Http.Result result = Http.Client.getRequest(uri);
+                return result;
+            }
+            
+            @Override
+            protected void onPostExecuteWithDismissDialog(Http.Result result) {
+                switch (result.statusCode) {
+                    // 問い合わせに成功したらユーザー情報を更新してメイン画面へ
+                    case HttpStatus.SC_OK:
                         try {
-                            String responseBody = httpClient.execute(request, responseHandler);
-                            JSONObject jsonObject = new JSONObject(responseBody);
-                            // ユーザー情報をプリファレンスに保存
-                            SharedPreferences pref = getSharedPreferences("user", MODE_PRIVATE);
-                            Editor editor = pref.edit();
+                            Editor editor = preferences.edit();
                             editor.putString("username", username);
                             editor.putString("password", password);
+                            JSONObject jsonObject = new JSONObject(result.responseBody);
                             editor.putInt("score", jsonObject.getInt("score"));
-                            // メイン画面に遷移
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(intent);
-                        }
-                        catch (ClientProtocolException e) {
-                            // ユーザーが存在しなければエラーの表示
-                            int status = ((HttpResponseException)e).getStatusCode();
-                            if (status == 404) {
-                                //((TextView)findViewById(R.id.textView1)).setText("user not found");
-                            }
-                        }
-                        catch (IOException e) {
-                            e.printStackTrace();
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-                        finally {
-                            httpClient.getConnectionManager().shutdown();
-                        }
-                        return null;
-                    }
-                    
-                    @Override
-                    protected void onPostExecute(Void v) {
-                        // 処理が終わったのでロード中のダイアログを終了させる
-                        progressDialog.dismiss();
-                    }
-                }.execute();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        break;
+                    // ユーザーがいなかったらエラーの表示
+                    case HttpStatus.SC_NOT_FOUND:
+                        ((TextView)findViewById(R.id.textView1)).setText("user not found");
+                        break;
+                    default:
+                        break;
+                }
             }
-        });
+        }.execute();
     }
 }
